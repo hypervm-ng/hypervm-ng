4b15a76db91af7d65388af27988e25b5080b6452
Krzysztof Taraszka <krzysztof.taraszka@hypervm-ng.org>
2015-12-06 19:27:15 +0100

    Fixed kernel module copying for copyKernelModules() function

--- hypervm/httpdocs/lib/vps/driver/vps__xenlib.php
+++ hypervm/httpdocs/lib/vps/driver/vps__xenlib.php
@@ -847,8 +847,8 @@ class vps__xen extends Lxdriverclass {
 		 */
 
 		$pygrub_record = explode('-', $this->main->ostemplate);
-		if (stripos($pygrub_record[3], 'pygrub') == TRUE) {
-                
+		if (!stripos($pygrub_record[3], 'pygrub') !== FALSE) {
+			
                     $mountpoint = $this->mount_this_guy();
 					if (is_centosfive()) {
 						$kernev = trim(`uname -r`);
