d792599f672770b3e9b699f83269556baee36ebf
Krzysztof Taraszka <krzysztof.taraszka@ippnet.pl>
2021-03-14 20:28:13 +0100

    Remove debug print

--- hypervm-install/hypervm-linux/lxins.php
+++ hypervm-install/hypervm-linux/lxins.php
@@ -202,7 +202,6 @@ function lxins_main()
     //
     // call script to install base OS templates and OpenVZ repo
     //
-    print("Calling: /usr/local/lxlabs/ext/php/php ../bin/install/virt-install.php --install-type=$installtype --virtualization-type=$virtualization $skiparg");
     passthru("/usr/local/lxlabs/ext/php/php ../bin/install/virt-install.php --install-type=$installtype --virtualization-type=$virtualization $skiparg");
 
 
